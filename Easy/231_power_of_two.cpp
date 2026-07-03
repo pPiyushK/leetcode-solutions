e7b87497ac4b655df5bdb8611c51846d577dc104
pPiyushK <kumarpiyush14024@gmail.com>
2026-07-03 06:34:09 +0000

    add problem no.231

---
+++ Easy/231_power_of_two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(long n) {
+       if(n==0){
+        return false;
+       }
+       else {
+         if((n&(n-1))==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+       }
+    }
+};
