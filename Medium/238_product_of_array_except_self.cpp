987739659a3abdfdf76e7b09d54c04520c6e4125
pPiyushK <kumarpiyush14024@gmail.com>
2026-07-16 18:01:24 +0530

    add problem no.238

---
+++ Medium/238_product_of_array_except_self.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n= nums.size(),prefix =1,suffix=1;
+        vector<int>ans(n,1);
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1]*nums[i-1];
+        }
+        for(int i=n-2; i>=0;i--){
+            suffix *=nums[i+1];
+            ans[i]*= suffix;
+        }
+        return ans;
+    }
+};
